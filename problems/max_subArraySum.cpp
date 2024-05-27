996669289bce9f80e51138bbce37f41ad7fa0a82
adityarathor007 <adityarathor120@gmail.com>
2024-05-27 21:53:03 +0530

    solved max_subArraySum streak continued(27May2024)

--- problems/max_subArraySum.cpp
+++ problems/max_subArraySum.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include <vector>
 using namespace std;
 
@@ -5,10 +6,40 @@ int maxSubarraySum(vector<int> arr)
 {
     // Complete this function, your function should return the maximum subarray sum
     int n = arr.size();
+    int max=0;
+    int c_sum=0;
+    for(int i=0;i<n;){
+        if(arr[i]>0){
+            while(c_sum>=0 and i<n){
+                c_sum+=arr[i];
+                cout<<"the current sum is "<<c_sum<<endl;
+                i++;
+                if(max<=c_sum){
+                    max=c_sum;
+                }
+            }
+           if(c_sum<0){
+            // i++;
+            c_sum=0;
+            continue;
+           } 
+        }
+        else{
+            i++;
+        }  
+    }
+
+    return max;
+
 }
 
 
 int main(){
 
+
+    vector <int> arr{-1,2,3,-6,7,8};
+    // vector <int> arr{-1,-2,-3};
+    cout<<maxSubarraySum(arr)<<endl;
+    return 0;
     
 }
