481a653a09e321fd6310719ff64e867a40088fa3
adityarathor007 <adityarathor120@gmail.com>
2024-07-23 06:19:56 +0530

    added input and output txt func

--- DP/2dDP/gameOfWits.cpp
+++ DP/2dDP/gameOfWits.cpp
@@ -2,27 +2,28 @@
 using namespace std;
 
 
-pair<char,int> helperFunc(string s,int st,int en,int ch){
-    if(st==en){
-        return {s[st],1};
-    }
-    if(ch%2==0){
-        if(s[st]=='H' and s[en]=='H'){
-            return {'H',0};
-        }
-        auto opt1=helperFunc(s,st+1,en,ch+1);
-        auto opt2=helperFunc(s,st,en-1,ch+1);
-        return {'O',max(opt1.second,opt2.second)};
+pair<char,int> helperFunc(string s,int st,int en){
+    // if(st==en){
+    //     return {s[st],1};
+    // }
+    // if(ch%2==0){
+    //     if(s[st]=='H' and s[en]=='H'){
+    //         return {'H',0};
+    //     }
+    //     auto opt1=helperFunc(s,st+1,en,ch+1);
+    //     auto opt2=helperFunc(s,st,en-1,ch+1);
+    //     return {'O',max(opt1.second,opt2.second)};
         
-    }
-    else{
-        if(s[st]=='O' and s[en]=='O'){
-            return {'O',0};
-        }
-        auto opt1=helperFunc(s,st+1,en,ch+1);
-        auto opt2=helperFunc(s,st,en-1,ch+1);
-        return {'T',max(opt1.second,opt2.second)}; 
-    }
+    // }
+    // else{
+    //     if(s[st]=='O' and s[en]=='O'){
+    //         return {'O',0};
+    //     }
+    //     auto opt1=helperFunc(s,st+1,en,ch+1);
+    //     auto opt2=helperFunc(s,st,en-1,ch+1);
+    //     return {'T',max(opt1.second,opt2.second)}; 
+    // }
+    
 }
 
 
---
+++ DP/testingAut.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printName(string name){
+    cout << "My name is " << name << endl;
+}
+
+
+int main(){
+  cout<<"Hello world"<<endl;
+    return 0;
+}
